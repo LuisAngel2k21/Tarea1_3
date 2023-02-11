ca463b79ad8e3fc6352462de8ec2a948553ffb6e
LuisAngel2k21 <luiscarranza198@yahoo.com>
2023-02-10 22:46:16 -0600

    Agregar archivos de proyecto.

---
+++ Tarea1_3/Tarea1_3/Datos/SQLiteDB.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using SQLite;
+
+namespace Tarea1_3.Datos
+{
+    public interface ISQLiteDB
+    {
+        SQLiteAsyncConnection GetConnection();
+    }
+}
